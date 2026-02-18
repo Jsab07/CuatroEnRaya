ddce63a665467b9f3f9a5d1f7ea7cea575f70189
Jsab07 <josesabiop07@gmail,com>
2026-02-18 14:18:19 +0100

    feat: Creación de la clase Casilla. Pasa todos los tests.

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -1,8 +1,33 @@
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
+import java.util.Objects;
+
 public class Casilla {
-    public Ficha ficha;
+    private Ficha ficha;
 
     public Casilla() {
+        ficha = null;
+    }
+
+    public Ficha getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(Ficha ficha) {
+        Objects.requireNonNull(ficha, "No se puede poner una ficha nula.");
+        if (estaOcupada()) {
+            throw new CuatroEnRayaExcepcion("La casilla ya contiene una ficha.");
+        }
+        this.ficha = ficha;
+    }
+
+    public boolean estaOcupada() {
+        ficha = getFicha();
+        return (ficha != null);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", (ficha == null) ? " " : ficha.toString());
     }
 }
