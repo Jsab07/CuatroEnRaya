bea85b37907d6500b5f33265ba6d46902dcd1226
Jsab07 <josesabiop07@gmail,com>
2026-02-23 00:43:47 +0100

    feat: Creación de la clase Jugador. Pasa todos los tests.

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
@@ -0,0 +1,34 @@
+package org.iesalandalus.programacion.cuatroenraya.modelo;
+
+import java.util.Objects;
+
+public record Jugador(String nombre, Ficha colorFichas) {
+
+    public Jugador {
+        validarNombre(nombre);
+        validarColorFichas(colorFichas);
+    }
+
+    private void validarNombre(String nombre) {
+        Objects.requireNonNull(nombre, "El nombre no puede ser nulo.");
+        if (nombre.strip().isEmpty()) {
+            throw new IllegalArgumentException("El nombre no puede estar en blanco.");
+        }
+    }
+
+    private void validarColorFichas(Ficha colorFichas) {
+        Objects.requireNonNull(colorFichas, "El color de las fichas no puede ser nulo.");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%s)", nombre, colorFichas);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Jugador jugador = (Jugador) o;
+        return Objects.equals(nombre, jugador.nombre) && colorFichas == jugador.colorFichas;
+    }
+}
