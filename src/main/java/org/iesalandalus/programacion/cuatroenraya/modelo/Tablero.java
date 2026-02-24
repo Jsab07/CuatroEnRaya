6c8114d34306ffce6763a4222bc9fbbb6dea74d9
Sabio_07 <josesabiop07@gmail.com>
2026-02-24 14:20:03 +0100

    fix: Corrección de la clase Tablero

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Tablero.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Tablero.java
@@ -1,18 +1,18 @@
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
 public class Tablero {
-    private Casilla[][] casillas;
-
     public static final int FILAS = 6;
     public static final int COLUMNAS = 7;
     public static final int FICHAS_CONSECUTIVAS = 4;
+
+    private Casilla[][] casillas;
 
     public Tablero() {
         casillas = new Casilla[FILAS, COLUMNAS];
     }
 
     private boolean columnaVacia(int columna) {
-        return casillas[FILAS][columna].estaOcupada();
+        return !casillas[0][columna].estaOcupada();
     }
 
     public boolean estaVacio()
